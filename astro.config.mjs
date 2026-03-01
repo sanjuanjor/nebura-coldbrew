e1df0570165f4bca8c75d20a619b4b2c17e7db8e
Sanjuanjor <sanjuanjor@gmail.com>
2026-03-01 15:34:18 +0100

    Add GitHub Pages auto-deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+const site = process.env.SITE;
+const base = process.env.BASE ?? '/';
+
+export default defineConfig({
+	site,
+	base,
+});
